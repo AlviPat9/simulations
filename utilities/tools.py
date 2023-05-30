ab49d875c0aaadb2c201ad03517aef5e56cc5205
Álvaro Marcos Canedo <79159554+AlviPat9@users.noreply.github.com>
2023-05-30 08:24:06 +0200

    Update tools.py

--- utilities/tools.py
+++ utilities/tools.py
@@ -7,7 +7,7 @@ This file defines useful functions thar are available to other projects and that
 Author: Alvaro Marcos Canedo
 """
 
-from numpy import cos, sin, sqrt, arctan2
+from numpy import cos, sin, sqrt, arctan2, ndarray, array, concatenate
 
 
 def haversine(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
@@ -34,4 +34,49 @@ def haversine(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
     return R * c
 
 
-__all__ = ["haversine"]
+def keep_angle_range(angle: float, min_angle: float, max_angle: float) -> ndarray:
+    """
+
+    Function to keep angle in a specified range.
+
+    @param angle: Angle to keep in specified range
+    @type angle: float
+    @param min_angle: Minimum value the angle can have.
+    @type min_angle: float
+    @param max_angle: Maximum value the angle can have.
+    @type max_angle: float
+
+    @return: Angle in specified range.
+    @rtype: float
+    """
+
+    range = max_angle - min_angle
+
+    normalize_angle = (angle - min_angle) % range
+
+    return normalize_angle + min_angle
+
+
+def convert_dict_to_ndarray(data: dict) -> ndarray:
+    """
+
+    Function to convert a dictionary to a numpy array format.
+
+    @param data: Data included in dictionary to convert to a numpy array.
+    @type data: dict
+
+    @return: Dictionary converted to a numpy array.
+    @rtype: ndarray
+    """
+
+    # Extract values from the dictionary.
+    dict_values = list(data.values())
+
+    # Convert numbers to numpy arrays.
+    dict_values = [array(val) if not isinstance(val, ndarray) else val for val in dict_values]
+
+    # Concatenate into a new numpy array and return the value
+    return concatenate(dict_values)
+
+
+__all__ = ["haversine", "keep_angle_range", "convert_dict_to_ndarray"]
